696cabd9b651a77643f3e59f4ce050ec7b65b785
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2021-11-02 20:35:43 -0700

    made it simpler

--- Neural_Networks_Follow_Along.py
+++ Neural_Networks_Follow_Along.py
@@ -8,10 +8,8 @@ bias1 = 2
 bias2 = 3
 bias3 = 0.5
 
-output1 = inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1
-output2 = inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2
-output3 = inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3
-
-output = [output1, output2, output3]
+output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1, 
+			inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+			inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
 
 print(output)
