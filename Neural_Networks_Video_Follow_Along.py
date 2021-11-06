2f2f404a4d6f37a37fb3a7ed97983e067dae2076
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2021-11-05 20:59:55 -0700

    Update Neural_Networks_Video_Follow_Along.py

--- Neural_Networks_Video_Follow_Along.py
+++ Neural_Networks_Video_Follow_Along.py
@@ -1,15 +1,29 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-inputs = [[1.0, 2.0, 3.0, 2.5],
-		  [2.0, 5.0, -1.0, 2.0],
-		  [-1.5, 2.7, 3.3, -0.8]]
+nnfs.init()
 
-weights = [[0.2, 0.8, -0.5, 1.0], 
-		   [0.5, -0.91, 0.26, -0.5], 
-		   [-0.26, -0.27, 0.17, 0.87]]
+X = [[1.0, 2.0, 3.0, 2.5],
+	 [2.0, 5.0, -1.0, 2.0],
+	 [-1.5, 2.7, 3.3, -0.8]]
 
-biases = [2, 3, 0.5]
+X, y = spiral_data(100, 3)
 
-output = np.dot(inputs, np.array(weights).T) + biases
+class Layer_Dense:
+	def __init__(self, n_inputs, n_neurons):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+		self.biases = np.zeros((1, n_neurons))
+	def forward(self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
+		
+class Activation_RelU:
+	def forward(self, inputs):
+		self.output = np.maximum(0, inputs)
 
-print(output)
+Layer1 = Layer_Dense(2, 5)
+Activation1 = Activation_RelU()
+
+Layer1.forward(X)
+Activation1.forward(Layer1.output)
+print(Activation1.output)
